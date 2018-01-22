fb2dc58dc9e7e2b740bafdf74706d9e6410eaae6
lpemake <petteri.makela@seamk.fi>
2018-01-22 10:28:56 +0200

    initial release without GUI client

---
+++ MeasurementLibrary/Measurements.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeasurementLibrary
+{
+    public class Measurements
+    {
+        public double Time { get; set; }
+        public double Pressure { get; set; }
+        public double Temperature { get; set; }
+
+        public Measurements() { }
+
+        public Measurements(string s)
+        {
+            string[] p = s.Split();
+            Time = double.Parse(p[0]);
+            Pressure = double.Parse(p[1]);
+            Temperature = double.Parse(p[2]);
+        }
+
+        public Measurements(double time, double p, double t)
+        {
+            Time = time;
+            Pressure = p;
+            Temperature = t;
+        }
+
+        public override string ToString()
+        {
+            return Time + " " + Pressure + " " + Temperature;
+        } 
+
+    }
+}
---
+++ Server/Simulator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using MeasurementLibrary;
+
+namespace Server
+{
+    
+
+    class Simulator
+    {
+        private Random gen = new Random();
+        Measurements measurement;
+        double time = 1;
+
+        public Measurements Read()
+        {
+            time += 1.0;
+            measurement = new Measurements();
+            measurement.Time = time;
+            measurement.Temperature = 10 + 5 * Math.Cos(time / 5.0) +
+                gen.NextDouble();
+            measurement.Pressure = 10 * Math.Sin(time) +
+                gen.NextDouble() * 2;
+
+            return measurement;
+        }
+    }
+
+
+}
